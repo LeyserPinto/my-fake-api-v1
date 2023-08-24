02ad89ee096202bca8e65fe5f6debcaddfdca1f4
Leyser Pinto <59316466+LeyserPinto@users.noreply.github.com>
2023-08-24 20:52:07 -0300

    :wrench: Vercel and Server Config

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db_food.json");
+const router = jsonServer.router("db/db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/restaurantes",
- })
-);
+
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
