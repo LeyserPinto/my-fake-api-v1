cf9b2a5b0b5e7945a90838dd6e94ad5e8addcbcd
Leyser Pinto <leyserpinto3@gmail.com>
2023-09-02 11:54:46 -0300

    :fix: Update Server

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 // JSON Server module
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router("db.json");
 
 // Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/restaurants": "/restaurants",
-  "/api/cardapios": "/cardapios",
- })
-);
+
 server.use(router);
 // Listen to port
 server.listen(3000, () => {
